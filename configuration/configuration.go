4fb49824622fc93beaeba80b7f9cf1e93b690dc3
Andrew Toulouse <git@atoulou.se>
2012-09-03 00:55:28 -0700

    Refactor configuration parsing into its own package

---
+++ configuration/configuration.go
@@ -0,0 +1,34 @@
+package configuration
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type Link struct {
+	Title       string
+	Url         string
+	Description string
+}
+
+type Configuration struct {
+	Theme       string
+	Name        string
+	Description string
+	Blogroll    []Link
+}
+
+func Load(config_path string) (config Configuration, err error) {
+	b, read_err := ioutil.ReadFile(config_path)
+
+	if read_err != nil {
+		return config, read_err
+	}
+
+	parse_err := json.Unmarshal(b, &config)
+	if parse_err != nil {
+		return config, parse_err
+	}
+
+	return config, err
+}
--- main.go
+++ main.go
@@ -1,38 +1,17 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
+	"plume/configuration"
 )
 
-type Link struct {
-	Title       string
-	Url         string
-	Description string
-}
-
-type Configuration struct {
-	Theme       string
-	Name        string
-	Description string
-	Blogroll    []Link
-}
-
 func main() {
-	b, err := ioutil.ReadFile("config.json")
+	conf, err := configuration.Load("config.json")
 
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	var m Configuration
-	parse_err := json.Unmarshal(b, &m)
-	if parse_err != nil {
-		log.Fatal(parse_err)
-		return
-	}
-
-	log.Print(m)
+	log.Print(conf)
 }
